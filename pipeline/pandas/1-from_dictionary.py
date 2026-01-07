8ba54d8c9b9c71896c39600154f9047ff8110576
Vugar Dunyamaliyev <vugar.dunyamaliyev.std@bhos.edu.az>
2026-01-07 20:23:09 +0400

    gitpusher

--- pipeline/pandas/1-from_dictionary.py
+++ pipeline/pandas/1-from_dictionary.py
@@ -1,11 +1,11 @@
+#!/usr/bin/env python3
+
 import pandas as pd
 
 """docstring lorem ipsum dolor sit amet
 holberton requires students to write
 useless stuff dot net"""
 
-my_dict = {"First": [0.0, 0.5, 1.0, 1.5], "Second": ["one", "two", "three", "four"]}
-
-def from_dictionary(dict=my_dict):
-    df = pd.DataFrame(my_dict, index=["A", 'B', 'C', 'D'])
-    return df
+my_dict = {"First": [0.0, 0.5, 1.0, 1.5],
+           "Second": ["one", "two", "three", "four"]}
+df = pd.DataFrame(my_dict, index=["A", 'B', 'C', 'D'])
--- pipeline/pandas/template.py
+++ pipeline/pandas/template.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 import pandas as pd
 
 """docstring lorem ipsum dolor sit amet
