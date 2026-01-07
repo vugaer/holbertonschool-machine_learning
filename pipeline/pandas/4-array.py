be047037e3e56142c9eff0e903973adc2735b3c8
Vugar Dunyamaliyev <vugar.dunyamaliyev.std@bhos.edu.az>
2026-01-07 23:20:10 +0400

    gitpusher

--- pipeline/pandas/4-array.py
+++ pipeline/pandas/4-array.py
@@ -1,12 +1,8 @@
-#!/usr/bin/env python3
-
-"""docstring lorem ipsum dolor sit amet
-holberton requires students to write
-useless stuff dot net"""
-
+""" module docstring lorem ipsum dolor sit ametitit is being the same as the other questionsplease do not disturb students via those docstrings"""
 
 def array(df):
-    """docstring lorem ipsum dolor sit amet
-    holberton requires students to write
-    useless stuff dot net"""
-    return df[['Open', 'Close']].iloc[-10:].to_numpy()
+    """asdasdasdasdasdasdasdasdasdasd
+    asdasdasdasdasdasdasdasdasdasda
+    asdasdasdasdasdasdasdasdasdasd"""
+    return df[['High', 'Close']][-10:].to_numpy()
+
