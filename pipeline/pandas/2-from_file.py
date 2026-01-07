5197429a12a40b2220d5b105ec4fc82a24912cf7
Vugar Dunyamaliyev <vugar.dunyamaliyev.std@bhos.edu.az>
2026-01-07 20:42:01 +0400

    gitpusher

--- pipeline/pandas/2-from_file.py
+++ pipeline/pandas/2-from_file.py
@@ -6,8 +6,8 @@ useless stuff dot net"""
 
 import pandas as pd
 
-
-def sum(arch):
+def from_file(filename, delimiter):
     """docstring lorem ipsum dolor sit amet
     holberton requires students to write
     useless stuff dot net"""
+    return pd.read_csv(filepath_or_buffer=filename, delimiter=delimiter)
