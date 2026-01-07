4878198f7a59af6391227b91220b021b53a8b874
Vugar Dunyamaliyev <vugar.dunyamaliyev.std@bhos.edu.az>
2026-01-08 01:09:50 +0400

    gitpusher

---
+++ pipeline/pandas/7-high.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+"""docstring lorem ipsum dolor sit amet
+holberton requires students to write
+useless stuff dot net"""
+
+import pandas as pd
+
+
+def high(df):
+    """docstring lorem ipsum dolor sit amet
+    holberton requires students to write
+    useless stuff dot net"""
+    return df.sort_values(ascending=False, inplace=True)
