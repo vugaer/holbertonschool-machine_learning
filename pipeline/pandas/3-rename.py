3d3d8e1069eb29affee4387801e9e83fcdf509e7
Vugar Dunyamaliyev <vugar.dunyamaliyev.std@bhos.edu.az>
2026-01-07 21:28:21 +0400

    gitpusher

--- pipeline/pandas/3-rename.py
+++ pipeline/pandas/3-rename.py
@@ -5,12 +5,11 @@ holberton requires students to write
 useless stuff dot net"""
 
 import pandas as pd
-from datetime import datetime
 
 
 def rename(df):
     """docstring lorem ipsum dolor sit amet
-    holberton requires students to write
+    holberton require s students to write
     useless stuff dot net"""
-    df['Timestamp'] = df['Timestamp'].apply(datetime.fromtimestamp)
+    df['Timestamp'] = pd.to_datetime(df['Timestamp'], unit='s')
     return df
