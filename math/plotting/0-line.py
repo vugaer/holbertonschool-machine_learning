6155ae42144002692d07720936e6efbbf8fcba6e
Vugar Dunyamaliyev <vugar.dunyamaliyev.std@bhos.edu.az>
2026-01-09 02:36:56 +0400

    gitpusher

--- math/plotting/0-line.py
+++ math/plotting/0-line.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env python3
+
+""" to be absolutely honest, what I am I have no idea
+what the absolute f**k are these, I mean honestly
+what absolute human prick would solve this question
+without savefig?"""
+
 import numpy as np
 import matplotlib.pyplot as plt
 
