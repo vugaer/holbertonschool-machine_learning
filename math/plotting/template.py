9b894e1650005b12950b61330ef3a48307557abf
Vugar Dunyamaliyev <vugar.dunyamaliyev.std@bhos.edu.az>
2026-01-09 02:38:59 +0400

    gitpusher

---
+++ math/plotting/template.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+""" to be absolutely honest, what I am I have no idea
+what the absolute f**k are these, I mean honestly
+what absolute human prick would solve this question
+without savefig?"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def line():
+    """ to be absolutely honest, what I am I have no idea
+    what the absolute f**k are these, I mean honestly
+    what absolute human prick would solve this question
+    without savefig?"""
+
+    plt.show()
+    plt.savefig('plot.png')
