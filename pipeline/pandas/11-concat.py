9a0f41475e952af0d5023c7cd6db0644a7d3506d
Vugar Dunyamaliyev <vugar.dunyamaliyev.std@bhos.edu.az>
2026-01-08 14:00:50 +0400

    gitpusher

---
+++ pipeline/pandas/11-concat.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+"""docstring lorem ipsum dolor sit amet
+holberton requires students to write
+useless stuff dot net"""
+
+index = __import__('10-index').index
+import pandas as pd
+
+
+def concat(df1, df2):
+    """docstring lorem ipsum dolor sit amet
+    holberton requires students to write
+    useless stuff dot net"""
+    # change index
+    df1 = index(df1)
+    df2 = index(df2)
+    # limit
+    df2_new = df2.loc[df2.index <= 1417411920]
+    result = pd.concat([df2_new, df1], keys=["bitstamp", "coinbase"])
+    return result
