d7717ff506d2eaf32f36edfe71dc46d06db658d0
Vugar Dunyamaliyev <vugar.dunyamaliyev.std@bhos.edu.az>
2026-01-09 02:37:41 +0400

    gitpusher

--- math/plotting/0-line.py
+++ math/plotting/0-line.py
@@ -10,7 +10,10 @@ import matplotlib.pyplot as plt
 
 
 def line():
-
+    """ to be absolutely honest, what I am I have no idea
+    what the absolute f**k are these, I mean honestly
+    what absolute human prick would solve this question
+    without savefig?"""
     y = np.arange(0, 11) ** 3
     plt.figure(figsize=(6.4, 4.8))
     x = np.arange(0, 11)
