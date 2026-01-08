585d1114bfbebd882eda52f82e072d6110b31c4a
Vugar Dunyamaliyev <vugar.dunyamaliyev.std@bhos.edu.az>
2026-01-08 04:01:40 +0400

    gitpusher

---
+++ pipeline/pandas/10-index.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+"""docstring lorem ipsum dolor sit amet
+holberton requires students to write
+useless stuff dot net"""
+
+
+def index(df):
+    """docstring lorem ipsum dolor sit amet
+    holberton requires students to write
+    useless stuff dot net"""
+    return df.set_index('Timestamp')
