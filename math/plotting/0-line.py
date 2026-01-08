5218d88518c8c562378f2865704b92e4b2cb2f41
Vugar Dunyamaliyev <vugar.dunyamaliyev.std@bhos.edu.az>
2026-01-09 02:35:37 +0400

    gitpusher

--- math/plotting/0-line.py
+++ math/plotting/0-line.py
@@ -2,14 +2,12 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 def line():
 
     y = np.arange(0, 11) ** 3
     plt.figure(figsize=(6.4, 4.8))
-
-    # my code
-
-    x = np.arange(0,11)
+    x = np.arange(0, 11)
     plt.plot(x, y, 'r')
     plt.xlim(0, 10)
     plt.show()
