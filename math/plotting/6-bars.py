8157545c15d645881ea3a9d767062453dde2790b
Vugar Dunyamaliyev <vugar.dunyamaliyev.std@bhos.edu.az>
2026-01-09 22:49:27 +0400

    gitpusher

--- math/plotting/6-bars.py
+++ math/plotting/6-bars.py
@@ -26,8 +26,8 @@ def bars():
     #plt.rcParams['lines.linewidth'] = 0.5
 
     # bar settings
-    plt.bar(names, data[0], color='r', label=fruits[0], width = 0.5)
-    plt.bar(names, data[1], color='y', width = 0.5,
+    plt.bar(names, data[0], color='red', label=fruits[0], width = 0.5)
+    plt.bar(names, data[1], color='yellow', width = 0.5,
             bottom=data[0], label=fruits[1])
     plt.bar(names, data[2], color='#ff8000', width = 0.5,
             bottom=data[0] + data[1], label=fruits[2])
