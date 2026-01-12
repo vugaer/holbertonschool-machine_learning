9df7fef7f68bca693269f40bfa88066ec4ed5af9
Vugar Dunyamaliyev <vugar.dunyamaliyev.std@bhos.edu.az>
2026-01-13 01:44:02 +0400

    gitpusher

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -5,8 +5,7 @@
 def summation_i_squared(n):
     """ we will create a program that will
     end future developer's j*b... let's go """
-    if (not isinstance(n, int)) and (n <= 0):
+    if (not isinstance(n, int)) or (n <= 0):
         return None
-    else:
-        squared = list(map(lambda x: x**2, range(1, int(n+1))))
-        return sum(squared)
+    squared = list(map(lambda x: x**2, range(1, int(n+1))))
+    return sum(squared)
