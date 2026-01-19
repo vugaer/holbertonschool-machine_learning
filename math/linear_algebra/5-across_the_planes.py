eac9bd73ff0dc4e11f09d5f7080e469e73dcbc63
Vugar Dunyamaliyev <vugar.dunyamaliyev.std@bhos.edu.az>
2026-01-20 01:06:38 +0400

    gitpusher

--- math/linear_algebra/5-across_the_planes.py
+++ math/linear_algebra/5-across_the_planes.py
@@ -6,7 +6,7 @@
 def add_matrices2D(arr1, arr2):
     """Look at the functions first,
     then this one! Good practice!"""
-    if any([arr1 == [[], []], arr2 == [[], []], arr1 == [], not arr1]):
+    if any([arr1 == list([], []), arr2 == list([], []), arr1 == [], not arr1, arr2 = []):
         return []
     elif not checkshape(arr1, arr2):
         return None
