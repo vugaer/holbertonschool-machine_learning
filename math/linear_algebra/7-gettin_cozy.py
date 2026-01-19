4bb8be4e98b9c4e5ba73ec498bb2365b719a41a3
Vugar Dunyamaliyev <vugar.dunyamaliyev.std@bhos.edu.az>
2026-01-20 02:59:42 +0400

    gitpusher

--- math/linear_algebra/7-gettin_cozy.py
+++ math/linear_algebra/7-gettin_cozy.py
@@ -8,7 +8,7 @@ def cat_matrices2D(mat1, mat2, axis=0):
     asasdasasdasdasdasdasdasdasd"""
     result = [row[:] for row in mat1]
     if axis == 0:
-        result += [_ for _ in mat2]
+        result += [row[:] for row in mat2]
     if axis == 1:
         for i in range(len(mat2)):
             result[i] += [mat2[i][0]]
