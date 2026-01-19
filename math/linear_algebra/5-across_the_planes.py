d942e821da1a0e48c5b206cd55696095b9713eb0
Vugar Dunyamaliyev <vugar.dunyamaliyev.std@bhos.edu.az>
2026-01-20 00:41:54 +0400

    gitpusher

---
+++ math/linear_algebra/5-across_the_planes.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+
+"""Module Documentation Lorem Ipsum"""
+
+
+def add_matrices2D(arr1, arr2):
+    """Look at the functions first,
+    then this one! Good practice!"""
+    if (arr1 == []) or (arr2 == []):
+        return []
+    elif not checkshape(arr1, arr2):
+        return None
+    else:
+        newarr = []
+        for i, j in zip(arr1, arr2):
+            newarr += [sum2dmatrix(i, j)]
+        return newarr
+
+
+def checkshape(arr1, arr2):
+    """We check the shape of two arrays.
+    To eliminate repetition, we import them"""
+    matrix_shape = __import__('2-size_me_please').matrix_shape
+    return (matrix_shape(arr1) == matrix_shape(arr2))
+
+
+def sum2dmatrix(arr1, arr2):
+    """ sum 2d matrixes with each other"""
+    if isinstance(arr1, list):
+        sumarr = arr1.copy()
+        for i in range(len(arr1)):
+            sumarr[i] = arr1[i] + arr2[i]
+        return sumarr
+    if isinstance(arr1, (int, float)):
+        return arr1 + arr2
