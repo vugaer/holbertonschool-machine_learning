8f7ccd5a0c6088116b73926114928a27116c0de5
Vugar Dunyamaliyev <vugar.dunyamaliyev.std@bhos.edu.az>
2026-01-19 21:58:24 +0400

    gitpusher

--- math/linear_algebra/4-line_up.py
+++ math/linear_algebra/4-line_up.py
@@ -4,14 +4,29 @@
 
 
 def add_arrays(arr1, arr2):
-    """Yuxularımı o atır suya, mənim üçün elə sino
-    gedir guya (:d) lorem ipsum dolor sit amet"""
+    """Look at the functions first,
+    then this one! Good practice!"""
     if not checkshape(arr1, arr2):
         return None
-    return np.array(arr1) + np.array(arr2)
+    newarr = []
+    for i, j in zip(arr1, arr2):
+        newarr += [sum2dmatrix(i, j)]
+    return newarr
+
 
 def checkshape(arr1, arr2):
-    """Yuxularımı o atır suya, mənim üçün elə sino
-    gedir yuya (:d) lorem ipsum dolor sit amet"""
+    """We check the shape of two arrays.
+    To eliminate repetition, we import them"""
     matrix_shape = __import__('2-size_me_please').matrix_shape
     return matrix_shape(arr1) == matrix_shape(arr2)
+
+
+def sum2dmatrix(arr1, arr2):
+    """ sum 2d matrixes with each other"""
+    if isinstance(arr1, list):
+        sumarr = arr1.copy()
+        for i in range(len(arr1)):
+            sumarr[i] = arr1[i] + arr2[i]
+        return sumarr
+    if isinstance(arr1, (int, float)):
+        return arr1 + arr2
