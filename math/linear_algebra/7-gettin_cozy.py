896965f48c1fc2fbe78f4328fe110e6380bb1fbb
Vugar Dunyamaliyev <vugar.dunyamaliyev.std@bhos.edu.az>
2026-01-20 02:20:06 +0400

    gitpusher

---
+++ math/linear_algebra/7-gettin_cozy.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+"adsadasadasdadasdasd"
+
+
+def cat_matrices2D(mat1, mat2, axis=0):
+    """asdasdasdasdasasasdasasd
+    asasdasasdasdasdasdasdasdasd"""
+    if not axis:
+        mat1.append(mat2, inplace=True)
+    if axis:
+        for i in range(len(mat2)):
+            mat1[i].append(mat2[i], inplace=True)
+    return mat1
