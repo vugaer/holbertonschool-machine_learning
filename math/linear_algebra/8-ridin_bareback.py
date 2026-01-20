20246662eb39325c49ed1eaee026e349a420381c
Vugar Dunyamaliyev <vugar.dunyamaliyev.std@bhos.edu.az>
2026-01-20 06:18:09 +0400

    gitpusher

--- math/linear_algebra/8-ridin_bareback.py
+++ math/linear_algebra/8-ridin_bareback.py
@@ -7,6 +7,8 @@ that we don't need in the future..."""
 def mat_mul(mat1, mat2):
     """some useless checker code for the
     checker to skip from me............."""
+    if len(mat1) == len(mat2):
+        return None
     rows = len(mat1)
     mid = len(mat1[0])
     cols = len(mat2[0])
