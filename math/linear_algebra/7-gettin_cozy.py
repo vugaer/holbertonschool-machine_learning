6fafe9e5cdbef16b3a79d6981b93da635c4144b9
Vugar Dunyamaliyev <vugar.dunyamaliyev.std@bhos.edu.az>
2026-01-20 05:27:41 +0400

    gitpusher

--- math/linear_algebra/7-gettin_cozy.py
+++ math/linear_algebra/7-gettin_cozy.py
@@ -5,7 +5,13 @@ OWN, THIS IS COMPLETELY BULLSHIT QUESTION BASED ON
 SOMEONE'S FUCKING STUPID EGO. I WASTED MY SHITTY
 5 FUCKING HOURS ON THAT STUPID IDIOTIC QUESTION
 WHILE WE JUST HAVE NP.CONCAT(). AT LEAST GIVE
-REASONABLE QUESTIONS NOT BULLSHIT LIKE THIS"""
+REASONABLE QUESTIONS NOT BULLSHIT LIKE THIS
+
+CORRECTION REQUESTS = 56
+WASTED_HOURS = 5
+JUST GO COPY AND PASTE MY SOL.
+
+"""
 
 
 def cat_matrices2D(m1, m2, axis=0):
@@ -13,28 +19,16 @@ def cat_matrices2D(m1, m2, axis=0):
     LOREM IPSUM DOLOR FUCK WHOEVER WROTE THIS Q"""
     result = [row[:] for row in m1]
     # Checker
-    #conditions = matdiff(m1) or matdiff(m2) or dimdiff(m1, m2, axis)
     if dimdiff(m1, m2, axis):
         return None
     # Main
     else:
         if not axis:  # axis == 0
-            result += [row[:] for row in m2]   # copy m2 rows
+            result += [row[:] for row in m2]
         elif axis:    # axis == 1
             for i in range(len(m1)):
-                result[i] += m2[i][:]          # copy slice
+                result[i] += m2[i][:]
     return result
-
-
-# def matdiff(m):
-#     """FUCK WHOEVER EVER THOUGHT OF THIS QUESTION
-#     WTF IS THAT??? ARE YOU INSANE? FUCK YOU!!!"""
-#     if not isinstance(m, list) or len(m) == 0:
-#         return True
-#     if not all(isinstance(row, list) for row in m):
-#         return True
-#     return False
-
 
 def dimdiff(m1, m2, axis):
     """LOREM IPSUM DOLOR FUCK WHOEVER WROTE THIS QUESTION
