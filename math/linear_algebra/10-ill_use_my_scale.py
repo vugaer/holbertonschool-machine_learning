19e060b465d09feee167fd886462d46d66b49dc5
Vugar Dunyamaliyev <vugar.dunyamaliyev.std@bhos.edu.az>
2026-01-20 07:03:18 +0400

    gitpusher

---
+++ math/linear_algebra/10-ill_use_my_scale.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+"""asdadasdasdasdadasdasd
+asdasdasdadasdasdasdasdasdasdd
+asdadasdasdadasdasdasdasdasdasd"""
+
+
+def np_shape(matrix):
+    """asdadasdasdadasdasdasdasdasda
+    asdasdasdasasdasdasdasdasdasdasd
+    asdasdasdasdasdasdasdasdasdasdasd"""
+    return matrix.shape
