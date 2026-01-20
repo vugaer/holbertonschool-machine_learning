10b4ac262b7d611e01f6360f42b00181d438e9f3
Vugar Dunyamaliyev <vugar.dunyamaliyev.std@bhos.edu.az>
2026-01-20 07:12:38 +0400

    gitpusher

---
+++ math/linear_algebra/13-cats_got_your_tongue.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+import numpy as np
+
+"""asadsasdasdasdasdasd
+asdasdasdasdadasdasdasdasd
+asdasdasdasdadasdasdasdasd"""
+
+
+def np_cat(mat1, mat2, axis=0):
+    """asadsasdasdasdasdasd
+    aaasdasdasdasdadasdasdasdasdasdasdasdasdad
+    asdasdasdasasdasdasdasdadasdasdasdasd
+    asdasdasdasdadasdasdasdasd"""
+    return np.concatenate((mat1, mat2), axis=axis)
