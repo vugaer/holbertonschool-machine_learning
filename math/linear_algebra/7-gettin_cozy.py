32f5e26207e48e57f71fea40ab1c5c1614e9dd1d
Vugar Dunyamaliyev <vugar.dunyamaliyev.std@bhos.edu.az>
2026-01-20 05:19:08 +0400

    gitpusher

--- math/linear_algebra/7-gettin_cozy.py
+++ math/linear_algebra/7-gettin_cozy.py
@@ -1,16 +1,16 @@
 #!/usr/bin/env python3
 
-"adsadasadasdadasdasd"
+"adsadasadasdadasdasdasdasdadadadsad
+asdasdasdadasdasdasdasdasdasdasdasasd
+WASTED_HOURS = 6 hours"
 
 
 def cat_matrices2D(m1, m2, axis=0):
     result = [row[:] for row in m1]
-
     # Checker
     conditions = matdiff(m1) or matdiff(m2) or dimdiff(m1, m2, axis)
     if conditions:
         return None
-
     # Main
     else:
         if not axis:  # axis == 0
@@ -18,31 +18,29 @@ def cat_matrices2D(m1, m2, axis=0):
         elif axis:    # axis == 1
             for i in range(len(m1)):
                 result[i] += m2[i][:]          # copy slice
-
     return result
-
 
 def matdiff(m):
     """FUCK WHOEVER EVER THOUGHT OF THIS QUESTION
-    WTF IS THAT??? ARE YOU INSANE?"""
+    WTF IS THAT??? ARE YOU INSANE? FUCK YOU!!!"""
     if not isinstance(m, list) or len(m) == 0:
         return True
     if not all(isinstance(row, list) for row in m):
         return True
 
-    # rectangular check (allow row length 0)
-    row_len = len(m[0])
-    for row in m:
-        if len(row) != row_len:
-            return True
+    # # rectangular check (allow row length 0)
+    # row_len = len(m[0])
+    # for row in m:
+    #     if len(row) != row_len:
+    #         return True
 
     return False
 
 
 def dimdiff(m1, m2, axis):
-    """Return True if dimensions do NOT match for concatenation."""
-    if axis not in (0, 1):
-        return True
+    # """Return True if dimensions do NOT match for concatenation."""
+    # if axis not in (0, 1):
+    #     return True
 
     if not axis:  # axis == 0: columns must match (including 0)
         return len(m1[0]) != len(m2[0])
