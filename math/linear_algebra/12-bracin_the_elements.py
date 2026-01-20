77ccb2cf6de7335586728e2bd6453ecf2998b8da
Vugar Dunyamaliyev <vugar.dunyamaliyev.std@bhos.edu.az>
2026-01-20 07:09:29 +0400

    gitpusher

---
+++ math/linear_algebra/12-bracin_the_elements.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+"""adsadasdadasdadasdasdasd
+adsadasdadasdadasdasdasd
+adsadasdadasdadasdasdasd"""
+
+
+def np_elementwise(mat1, mat2):
+    """adsadasdadasdadasdasdasd
+    adsadasdadasdadasdasdasd
+    adsadasdadasdadasdasdasd"""
+    return mat1 + mat2, mat1 - mat2, mat1 * mat2, mat1 / mat2
