6341c48de89a2d9e9853b9fd5805d47c4724492d
Vugar Dunyamaliyev <vugar.dunyamaliyev.std@bhos.edu.az>
2026-01-20 06:21:44 +0400

    gitpusher

--- math/linear_algebra/8-ridin_bareback.py
+++ math/linear_algebra/8-ridin_bareback.py
@@ -7,15 +7,14 @@ that we don't need in the future..."""
 def mat_mul(mat1, mat2):
     """some useless checker code for the
     checker to skip from me............."""
-    if len(mat1) == len(mat2):
+    if len(mat1) != len(mat2):
         return None
-    else:
-        rows = len(mat1)
-        mid = len(mat1[0])
-        cols = len(mat2[0])
-        result = [[0 for _ in range(cols)] for _ in range(rows)]
-        for i in range(rows):
-            for j in range(cols):
-                for k in range(mid):
-                    result[i][j] += mat1[i][k] * mat2[k][j]
+    rows = len(mat1)
+    mid = len(mat1[0])
+    cols = len(mat2[0])
+    result = [[0 for _ in range(cols)] for _ in range(rows)]
+    for i in range(rows):
+        for j in range(cols):
+            for k in range(mid):
+                result[i][j] += mat1[i][k] * mat2[k][j]
     return result
