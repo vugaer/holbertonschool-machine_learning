fc80038523e8c5363ed178ff63a63dd00c45c265
Vugar Dunyamaliyev <vugar.dunyamaliyev.std@bhos.edu.az>
2026-01-20 05:08:35 +0400

    gitpusher

--- math/linear_algebra/7-gettin_cozy.py
+++ math/linear_algebra/7-gettin_cozy.py
@@ -23,11 +23,26 @@ def cat_matrices2D(m1, m2, axis=0):
 
 def colrowdiff(m2, axis):
     """Returns True if m2 has inconsistent shape (ragged), else False"""
+
+    # axis must be 0 or 1
+    if axis not in (0, 1):
+        return True
+
+    # must be a list (matrix)
+    if not isinstance(m2, list):
+        return True
+
+    # empty matrix is invalid
+    if len(m2) == 0:
+        return True
+
     lengths = []
 
     for row in m2:
+        # each row must be a list
         if not isinstance(row, list):
             return True
+
         lengths.append(len(row))
 
     # if any row is empty -> invalid matrix
