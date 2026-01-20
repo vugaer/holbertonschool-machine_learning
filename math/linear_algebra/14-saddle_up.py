2afa650bb4b747a29c14915b6aef6fe796b89a75
Vugar Dunyamaliyev <vugar.dunyamaliyev.std@bhos.edu.az>
2026-01-20 07:15:25 +0400

    gitpusher

---
+++ math/linear_algebra/14-saddle_up.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+"""asadsasdasdasdasdasd
+asasdasdasdasdadasdasdasdasd
+asdadasdadsdadasdasdasdasd"""
+import numpy as np
+
+
+def np_matmul(mat1, mat2):
+    """asadsasdasdasdasdasd
+    aaasdasdasdasdadasdasdasd
+    asdasdasdasdasdadasdasdasd
+    asasdasdasdasdadasdasdasdasd
+    asdasdasdasdadasdasdasdasd"""
+    return np.matmul(mat1, mat2)
