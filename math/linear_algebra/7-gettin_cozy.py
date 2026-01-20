3357639e12cd0a55add9ebaf73e01a1236dcd182
Vugar Dunyamaliyev <vugar.dunyamaliyev.std@bhos.edu.az>
2026-01-20 04:05:51 +0400

    gitpusher

--- math/linear_algebra/7-gettin_cozy.py
+++ math/linear_algebra/7-gettin_cozy.py
@@ -10,6 +10,6 @@ def cat_matrices2D(m1, m2, axis=0):
     if axis:     
         for i in range(len(m2)):
             result[i] += m2[i]
-    print('#############################')
-    print(f'm1={m1}\nm2={m2}\naxis={axis}')
+    # print('#############################')  # for Debug
+    # print(f'm1={m1}\nm2={m2}\naxis={axis}')
     return result
