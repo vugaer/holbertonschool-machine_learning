ad90b2a7aee88a2d2882bd9416bb8e0a1b89e36e
Vugar Dunyamaliyev <vugar.dunyamaliyev.std@bhos.edu.az>
2026-01-20 05:45:56 +0400

    gitpusher

---
+++ math/linear_algebra/8-ridin_bareback.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+""" nice good job let's learn some stuff
+that we don't need in the future..."""
+
+
+def mat_mul(mat1, mat2):
+    rows = len(mat1)
+    mid = len(mat1[0])
+    cols = len(mat2[0])
+    result = [[0 for _ in range(cols)] for _ in range(rows)]
+    for i in range(rows):
+        for j in range(cols):
+            for k in range(mid):
+                result[i][j] += mat1[i][k] * mat2[k][j]
+    return result
