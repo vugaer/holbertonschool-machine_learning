a570aad4e62482ca7b798b5cf941036f6fec0f80
Vugar Dunyamaliyev <vugar.dunyamaliyev.std@bhos.edu.az>
2026-01-20 07:04:53 +0400

    gitpusher

---
+++ math/linear_algebra/11-the_western_exchange.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+"""asdadadasdadasdasdadasd
+asdasdasdasdasdasdadasdasdasd
+asdasdasdasdasdasdasdasdasdasd
+asdasdasdadasdasdasdadasdadsad"""
+
+def np_transpose(matrix):
+    return matrix.T
