c6920207e2bdfde7fe48ada2fd0b92cffbe108ad
Vugar Dunyamaliyev <vugar.dunyamaliyev.std@bhos.edu.az>
2026-01-20 04:10:06 +0400

    gitpusher

--- math/linear_algebra/7-gettin_cozy.py
+++ math/linear_algebra/7-gettin_cozy.py
@@ -5,11 +5,17 @@
 
 def cat_matrices2D(m1, m2, axis=0):
     result = [row[:] for row in m1]
-    if not axis:
+    if not checker:
+        return None
+    elif not axis:
         result += [m2[i] for i in range(len(m2))]
-    if axis:     
+    elif axis:     
         for i in range(len(m2)):
             result[i] += m2[i]
     # print('#############################')  # for Debug
     # print(f'm1={m1}\nm2={m2}\naxis={axis}')
     return result
+
+def checker(m1, m2):
+    matrix_shape = __import__('2-size_me_please').matrix_shape
+    return matrix_shape(m1) == matrix_shape(m2)
