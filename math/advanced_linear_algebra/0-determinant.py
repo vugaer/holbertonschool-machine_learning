6789fc9b7a5d3151d50a48875eb49082f1391b2c
Vugar Dunyamaliyev <vugar.dunyamaliyev.std@bhos.edu.az>
2026-01-30 20:43:25 +0400

    gitpusher

--- math/advanced_linear_algebra/0-determinant.py
+++ math/advanced_linear_algebra/0-determinant.py
@@ -14,7 +14,35 @@ def determinant(matrix):
         raise TypeError("matrix must be a list of lists")
     elif not all(len(srow) == len(matrix) for srow in matrix):
         raise ValueError("matrix must be a square matrix")
-    if len(matrix[0]) == 1:
+
+    n = len(matrix)
+
+    if n == 1:
         return matrix[0][0]
-    else:
-        return round(numpy.linalg.det(matrix))
+
+    if n == 2:
+        return matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0]
+
+    det = 0
+    j = 0
+    while j < n:
+        a = matrix[0][j]
+
+        minor = []
+        r = 1
+        while r < n:
+            row = []
+            c = 0
+            while c < n:
+                if c != j:
+                    row.append(matrix[r][c])
+                c += 1
+            minor.append(row)
+            r += 1
+
+        sign = -1 if (j % 2 == 1) else 1
+
+        det += sign * a * determinant(minor)
+        j += 1
+
+    return det
