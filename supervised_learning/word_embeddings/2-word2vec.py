32a9ccee95b5b843606dc99a1e4031aa6007fd84
Vugar Dunyamaliyev <vugar.dunyamaliyev@outlook.com>
2026-08-01 21:56:04 +0400

    asd

--- supervised_learning/word_embeddings/2-word2vec.py
+++ supervised_learning/word_embeddings/2-word2vec.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
-"""Train a Word2Vec model."""
+"""
+A script that contains the function word2vec_model()
+"""
 
 import gensim
 
@@ -7,23 +9,30 @@ import gensim
 def word2vec_model(sentences, vector_size=100, min_count=5,
                    window=5, negative=5, cbow=True,
                    epochs=5, seed=0, workers=1):
-    """Create, build, and train a Word2Vec model."""
+    """
+    A function that creates and trains a gensim Word2Vec model:
+    """
+    # Set the training algorithm based on cbow parameter
+    sg = 0 if cbow else 1
+
+    # Create the Word2Vec model
     model = gensim.models.Word2Vec(
+        sentences=sentences,
         vector_size=vector_size,
         min_count=min_count,
         window=window,
         negative=negative,
-        sg=0 if cbow else 1,
+        sg=sg,
+        epochs=epochs,
         seed=seed,
         workers=workers
-    )
+        )
 
+    # prepare the model vocabulary
     model.build_vocab(sentences)
 
-    model.train(
-        sentences,
-        total_examples=model.corpus_count,
-        epochs=epochs
-    )
+    # Train the model
+    model.train(sentences, total_examples=model.corpus_count,
+                epochs=model.epochs)
 
     return model
