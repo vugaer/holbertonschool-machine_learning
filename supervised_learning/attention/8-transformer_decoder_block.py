ef9c7b482fe22f535f33bb0fc4a0c0a09d9462e1
Vugar Dunyamaliyev <vugar.dunyamaliyev@outlook.com>
2026-08-01 20:51:54 +0400

    asd

---
+++ supervised_learning/attention/8-transformer_decoder_block.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python3
+import tensorflow as tf
+
+MultiHeadAttention = __import__('6-multihead_attention').MultiHeadAttention
+
+
+class DecoderBlock(tf.keras.layers.Layer):
+    """Transformer Decoder Block."""
+
+    def __init__(self, dm, h, hidden, drop_rate=0.1):
+        super().__init__()
+
+        self.mha1 = MultiHeadAttention(dm, h)
+        self.mha2 = MultiHeadAttention(dm, h)
+
+        self.dense_hidden = tf.keras.layers.Dense(
+            hidden,
+            activation='relu'
+        )
+        self.dense_output = tf.keras.layers.Dense(dm)
+
+        self.layernorm1 = tf.keras.layers.LayerNormalization(
+            epsilon=1e-6
+        )
+        self.layernorm2 = tf.keras.layers.LayerNormalization(
+            epsilon=1e-6
+        )
+        self.layernorm3 = tf.keras.layers.LayerNormalization(
+            epsilon=1e-6
+        )
+
+        self.dropout1 = tf.keras.layers.Dropout(drop_rate)
+        self.dropout2 = tf.keras.layers.Dropout(drop_rate)
+        self.dropout3 = tf.keras.layers.Dropout(drop_rate)
+
+    def call(self, x, encoder_output, training,
+             look_ahead_mask, padding_mask):
+        """Forward pass."""
+
+        # Masked self-attention
+        attn1, _ = self.mha1(
+            x,
+            x,
+            x,
+            look_ahead_mask
+        )
+        attn1 = self.dropout1(
+            attn1,
+            training=training
+        )
+        out1 = self.layernorm1(x + attn1)
+
+        # Encoder-decoder attention
+        attn2, _ = self.mha2(
+            out1,
+            encoder_output,
+            encoder_output,
+            padding_mask
+        )
+        attn2 = self.dropout2(
+            attn2,
+            training=training
+        )
+        out2 = self.layernorm2(out1 + attn2)
+
+        # Feed-forward network
+        ffn = self.dense_hidden(out2)
+        ffn = self.dense_output(ffn)
+        ffn = self.dropout3(
+            ffn,
+            training=training
+        )
+
+        return self.layernorm3(out2 + ffn)
