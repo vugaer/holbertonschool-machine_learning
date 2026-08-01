5031da6f8b3347bae4b696897d7d3dd562bdf201
Vugar Dunyamaliyev <vugar.dunyamaliyev@outlook.com>
2026-08-01 20:48:18 +0400

    asd

--- supervised_learning/attention/7-transformer_encoder_block.py
+++ supervised_learning/attention/7-transformer_encoder_block.py
@@ -1,34 +1,22 @@
 #!/usr/bin/env python3
-"""Transformer Encoder Block"""
-
 import tensorflow as tf
 
-MultiHeadAttention = __import__(
-    "6-multihead_attention"
-).MultiHeadAttention
+MultiHeadAttention = __import__('6-multihead_attention').MultiHeadAttention
 
 
 class EncoderBlock(tf.keras.layers.Layer):
-    """Creates a Transformer encoder block."""
+    """Transformer encoder block."""
 
     def __init__(self, dm, h, hidden, drop_rate=0.1):
-        """
-        Class constructor.
-
-        Args:
-            dm: dimensionality of the model
-            h: number of attention heads
-            hidden: number of hidden units in the feed-forward layer
-            drop_rate: dropout rate
-        """
         super().__init__()
 
         self.mha = MultiHeadAttention(dm, h)
 
         self.dense_hidden = tf.keras.layers.Dense(
             hidden,
-            activation='relu'
+            activation="relu"
         )
+
         self.dense_output = tf.keras.layers.Dense(dm)
 
         self.layernorm1 = tf.keras.layers.LayerNormalization(
@@ -42,23 +30,12 @@ class EncoderBlock(tf.keras.layers.Layer):
         self.dropout2 = tf.keras.layers.Dropout(drop_rate)
 
     def call(self, x, training, mask=None):
-        """
-        Forward pass.
+        """Forward pass."""
 
-        Args:
-            x: input tensor of shape (batch, input_seq_len, dm)
-            training: boolean indicating training mode
-            mask: attention mask
-
-        Returns:
-            Tensor of shape (batch, input_seq_len, dm)
-        """
-        # Multi-head attention
         attn_output, _ = self.mha(x, x, x, mask)
         attn_output = self.dropout1(attn_output, training=training)
         out1 = self.layernorm1(x + attn_output)
 
-        # Feed-forward network
         ffn_output = self.dense_hidden(out1)
         ffn_output = self.dense_output(ffn_output)
         ffn_output = self.dropout2(ffn_output, training=training)
