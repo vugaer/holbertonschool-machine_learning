12d01842051aed3db95f41ba41f9549066f12ea0
Vugar Dunyamaliyev <vugar.dunyamaliyev@outlook.com>
2026-08-01 21:57:10 +0400

    asd

--- supervised_learning/word_embeddings/4-fasttext.py
+++ supervised_learning/word_embeddings/4-fasttext.py
@@ -1,22 +1,38 @@
 #!/usr/bin/env python3
-"""FastText model"""
+"""
+A script that contains the function fasttext_model()
+"""
 
 import gensim
 
 
-def fasttext_model(sentences, vector_size=100, min_count=5, negative=5,
-                   window=5, cbow=True, epochs=5, seed=0, workers=1):
-    """Creates and trains a FastText model."""
+def fasttext_model(sentences, vector_size=100, min_count=5,
+                   window=5, negative=5, cbow=True,
+                   epochs=5, seed=0, workers=1):
+    """
+    A function that creates and trains a gensim FastText model:
+    """
+    # Set the training algorithm based on cbow parameter
+    sg = 0 if cbow else 1
+
+    # Create the FastText model
     model = gensim.models.FastText(
         sentences=sentences,
         vector_size=vector_size,
         min_count=min_count,
         window=window,
         negative=negative,
-        sg=0 if cbow else 1,
+        sg=sg,
         epochs=epochs,
         seed=seed,
         workers=workers
-    )
+        )
+
+    # prepare the model vocabulary
+    model.build_vocab(sentences)
+
+    # Train the model
+    model.train(sentences, total_examples=model.corpus_count,
+                epochs=model.epochs)
 
     return model
